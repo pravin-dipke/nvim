00d7426b6436ff2cfbd4e860677b297fe80d5c5a
Pravin Dipke <pravinbdipke@gmail.com>
2024-03-18 23:51:17 +0530

    new remaps added

--- lua/itspravin/remap.lua
+++ lua/itspravin/remap.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+----------------------
+--- Plugin Keybinds ---
+----------------------
+
+-- vim-maximizer
+vim.keymap.set("n", "<leader>mt", ":MaximizerToggle<CR>") -- toggle split window maximization
