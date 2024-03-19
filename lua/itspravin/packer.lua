36a86489cbc5c236790b3e4013026fb147ab23d8
Pravin Dipke <pravinbdipke@gmail.com>
2024-03-19 14:53:43 +0530

    files modified

--- lua/itspravin/packer.lua
+++ lua/itspravin/packer.lua
@@ -1,66 +1,97 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    --use { 'rose-pine/neovim', as = 'rose-pine', config = function()
-        --vim.cmd('colorscheme rose-pine')
-        --end
-        --}
-        --use { "catppuccin/nvim", as = "catppuccin" }
-        use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-        use "rebelot/kanagawa.nvim"
-        use("mbbill/undotree")
-        use {"akinsho/toggleterm.nvim", tag = '*', config = function()
-            require("toggleterm").setup()
-        end}
-        --goyo.vim for distraction-free writing in vim.
-        use 'junegunn/goyo.vim'
-        -- managing & installing lsp servers, linters & formatters
-        use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
-        use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
-        -- configuring lsp servers
-        use("neovim/nvim-lspconfig") -- easily configure language servers
-        use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
-        use({
-            "glepnir/lspsaga.nvim",
-            branch = "main",
-            requires = {
-                { "nvim-tree/nvim-web-devicons" },
-                { "nvim-treesitter/nvim-treesitter" },
-            },
-        }) -- enhanced lsp uis
-        use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
-        use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
-        use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	-- fuzzy finding w/ telescope
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+	--use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
 
-        use("szw/vim-maximizer") -- maximizes and restores current window
+	--use { 'rose-pine/neovim', as = 'rose-pine', config = function()
+	--vim.cmd('colorscheme rose-pine')
+	--end
+	--}
+	--use { "catppuccin/nvim", as = "catppuccin" }
 
-        -- essential plugins
-        use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
-        --use("inkarkat/vim-ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
+	-- treesitter configuration
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
 
-        -- commenting with gcc
-        use {
-            'numToStr/Comment.nvim',
-            config = function()
-                require('Comment').setup()
-            end
-        }
+	use("rebelot/kanagawa.nvim")
+	use("mbbill/undotree")
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "*",
+		config = function()
+			require("toggleterm").setup()
+		end,
+	})
 
-        -- vs-code like icons
-        use("nvim-tree/nvim-web-devicons")
+	--goyo.vim for distraction-free writing in vim.
+	use("junegunn/goyo.vim")
 
+	-- managing & installing lsp servers, linters & formatters
+	use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
+	use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+
+	-- configuring lsp servers
+	use("neovim/nvim-lspconfig") -- easily configure language servers
+	use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+	use({
+		"glepnir/lspsaga.nvim",
+		branch = "main",
+		requires = {
+			{ "nvim-tree/nvim-web-devicons" },
+			{ "nvim-treesitter/nvim-treesitter" },
+		},
+	}) -- enhanced lsp uis
+	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
+	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+
+	-- formatting & linting
+	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+
+	use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
+
+	use("szw/vim-maximizer") -- maximizes and restores current window
+
+	-- essential plugins
+	use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
+	--use("inkarkat/vim-ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
+
+	-- commenting with gcc
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
+
+	-- vs-code like icons
+	use("nvim-tree/nvim-web-devicons")
+
+	-- autocompletion
+	use("hrsh7th/nvim-cmp") -- completion plugin
+	use("hrsh7th/cmp-buffer") -- source for text in buffer
+	use("hrsh7th/cmp-path") -- source for file system paths
+
+	-- snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+	use("rafamadriz/friendly-snippets") -- useful snippets
 end)
-
-
-
-
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -80,10 +80,35 @@ _G.packer_plugins = {
     path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/Comment.nvim",
     url = "https://github.com/numToStr/Comment.nvim"
   },
+  LuaSnip = {
+    loaded = true,
+    path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    url = "https://github.com/L3MON4D3/LuaSnip"
+  },
+  ["cmp-buffer"] = {
+    loaded = true,
+    path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    url = "https://github.com/hrsh7th/cmp-buffer"
+  },
   ["cmp-nvim-lsp"] = {
     loaded = true,
     path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
+  },
+  ["cmp-path"] = {
+    loaded = true,
+    path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/cmp-path",
+    url = "https://github.com/hrsh7th/cmp-path"
+  },
+  cmp_luasnip = {
+    loaded = true,
+    path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    url = "https://github.com/saadparwaiz1/cmp_luasnip"
+  },
+  ["friendly-snippets"] = {
+    loaded = true,
+    path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    url = "https://github.com/rafamadriz/friendly-snippets"
   },
   ["goyo.vim"] = {
     loaded = true,
@@ -110,20 +135,30 @@ _G.packer_plugins = {
     path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
+  ["mason-null-ls.nvim"] = {
+    loaded = true,
+    path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/mason-null-ls.nvim",
+    url = "https://github.com/jayp0521/mason-null-ls.nvim"
+  },
   ["mason.nvim"] = {
     loaded = true,
     path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
+  ["null-ls.nvim"] = {
+    loaded = true,
+    path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
+    url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
+  },
+  ["nvim-cmp"] = {
+    loaded = true,
+    path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    url = "https://github.com/hrsh7th/nvim-cmp"
+  },
   ["nvim-lspconfig"] = {
     loaded = true,
     path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
-  },
-  ["nvim-tree.lua"] = {
-    loaded = true,
-    path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
-    url = "https://github.com/nvim-tree/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
@@ -144,6 +179,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
+  },
+  ["telescope-fzf-native.nvim"] = {
+    loaded = true,
+    path = "/Users/PravinDipke/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
+    url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
