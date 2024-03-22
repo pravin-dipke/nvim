ec9ce7a27882b3289fd3c2a3b1d58976c63f57a8
Pravin Dipke <pravinbdipke@gmail.com>
2024-03-22 21:13:34 +0530

    configuration files modified

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,14 +1,13 @@
-require('kanagawa').setup({
-    disable_background = true
+require("kanagawa").setup({
+	disable_background = true,
 })
 
-function ColorMyPencils(color) 
+function ColorMyPencils(color)
 	color = color or "kanagawa"
 	vim.cmd.colorscheme(color)
 
 	--vim.api.nvim_set_hl(0, "nvim_set_hlNormal", { bg = "none" })
 	--vim.api.nvim_set_hl(0, "nvim_set_hlNormalFloat", { bg = "none" })
-
 end
 
 ColorMyPencils()
